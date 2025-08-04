bbb261b926e75b4af5fcc6f2ebcd9a0b93d0d56d
aaallii <Aliburakplaying@gmail.com>
2025-08-04 11:53:29 +0300

    Update engine.js
    update

--- engine.js
+++ engine.js
@@ -1,1 +1,14 @@
-const j=1
+function loadScreen(){
+  const camera = { x: 50, y: 50, z: 50 };
+  fetch("nodes.json")
+  .then(response=>response.json)
+  .then(data=>{
+    for(let i=1,data[String(i)],i++){
+      let pageData=data[String(i)]
+      let x=0
+      let z=0
+      let y=0
+    if(Array.isArray(pageData){
+        
+      }}})
+}
